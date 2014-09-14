b74e104ac71f527e0b016966964370ff152806a1
ismc2222 <pawel@fibrotrust.org>
2014-09-14 20:43:43 +0100

    arranged the code

--- FeaturesOut.java
+++ FeaturesOut.java
@@ -9,29 +9,24 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
-
 public class FeaturesOut{
 
-
 	public static void main(String[] args)
-
 	{
-
-
 		String filePath = "Features.txt";
 		String filePath2 = "FeaturesToDelete.txt";
 		BufferedReader br,br2;
 		String line = "";
 
 		HashMap<String,String> numbers = new HashMap<String,String>();
+		
 		try {
 
 			br = new BufferedReader(new FileReader(filePath));
+			br2 = new BufferedReader(new FileReader(filePath2));
 			try {
 				while((line = br.readLine()) != null)
 				{
-
 					//Splits single line into words
 					String[] words = line.split(",",-1);//adress empty col with -1
 					String UDP = words[0];
@@ -43,70 +38,60 @@ public class FeaturesOut{
 					numbers.put(UDP, rest);
 
 				}
-				br.close();
-
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			br = new BufferedReader(new FileReader(filePath2));
-			try {
-				while((line = br.readLine()) != null)
+				
+				System.out.println("Before Delete: "+ numbers.size());
+				
+				while((line = br2.readLine()) != null)
 				{
 
-					if (numbers.containsKey(line)){numbers.remove(line);}else{
-
-
-
+					if (numbers.containsKey(line)){
+						numbers.remove(line);
 					}
-
-
 				}
-				if (numbers.containsKey("10713959"))
-					System.out.println("success");
-
-				System.out.println(numbers.size());
-				// System.out.println(names.size());
-
-				FileWriter fstream;
-				BufferedWriter out;
-
-				// create your filewriter and bufferedreader
-				fstream = new FileWriter("WithoutToDelete.txt");
-				out = new BufferedWriter(fstream);
-
-				// initialize the record count
-				//int count = 0;
-
-				// create your iterator for your map
-				Iterator<Entry<String, String>> it = numbers.entrySet().iterator();
-
-				// then use the iterator to loop through the map, stopping when we reach the
-				// last record in the map
-				while (it.hasNext()) {
-
-					// the key/value pair is stored here in pairs
-					Map.Entry<String, String> pairs = it.next();
-					//System.out.println(pairs.getKey() + pairs.getValue());
-
-					// since you only want the value, we only care about pairs.getValue(), which is written to out
-					out.write(pairs.getKey()+","+pairs.getValue() + "\n");
-
-					// increment the record count once we have printed to the file
-					//count++;
-				}
-				// lastly, close the file and end
-				out.close();
-
-				br.close();
-
 
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
-		} catch (FileNotFoundException e) {
+			/*Write numbers to file */
+			
+			System.out.println("After Delete: "+ numbers.size());
+			System.out.println("File named 'WithoutToDelete.txt' "
+					+ "\nwith remaining data is in the project root");
+
+			FileWriter fstream;
+			BufferedWriter out;
+
+			// create your filewriter and bufferedreader
+			fstream = new FileWriter("WithoutToDelete.txt");
+			out = new BufferedWriter(fstream);
+
+			// initialize the record count
+			//int count = 0;
+
+			// create your iterator for your map
+			Iterator<Entry<String, String>> it = numbers.entrySet().iterator();
+
+			// then use the iterator to loop through the map, stopping when we reach the
+			// last record in the map
+			while (it.hasNext()) {
+
+				// the key/value pair is stored here in pairs
+				Map.Entry<String, String> pairs = it.next();
+				//System.out.println(pairs.getKey() + pairs.getValue());
+
+				// since you only want the value, we only care about pairs.getValue(), which is written to out
+				out.write(pairs.getKey()+","+pairs.getValue() + "\n");
+
+			}
+			// lastly, close the file and end
+			out.close();
+
+			br.close();
+			br2.close();
+
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
